8951ae256c6eb217907e46084f327589e21d0040
AndrewIndeche <indecheandrew@gmail.com>
2021-09-05 16:48:55 +0300

    Add self instance of array

--- user.py
+++ user.py
@@ -1,7 +1,11 @@
 class UserName:
 """
-Class that generates new usernames
+Class that generates new UserNames.
 
 """
-
+Userlist= []  #Array of empty user list
 def_init_(self,name,platform,password)
+
+self.name=name
+self.platform=platform
+self.password=password
