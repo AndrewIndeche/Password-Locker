932f354fb05ca284dd676a427f7ce2ac234a4095
AndrewIndeche <indecheandrew@gmail.com>
2021-09-05 20:28:05 +0300

    Add preceding tests

--- user-test.py
+++ user-test.py
@@ -1,16 +1,39 @@
-import unitest
+import unittest
 from user import User
 
-Class TestUser(unitest.Testcase):
-
+class TestUser(unittest.Testcase):
     '''
     Test class that defines test cases for user class behaviours.
 
     Args:
-        unitest.Testcase: TestCase class that helps in creating test cases
+        unittest.Testcase: TestCase class that helps in creating test cases
     '''
-def setUp(self):
-    '''
-    Set up method to run before each test cases.
-    '''
-    self.new_user = User ("Allan","Davis","Facebook")
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User ("Allan","Facebook","Davis")
+
+    def test_init(self):
+            '''
+            Test case to test if the object is initialied properly
+            '''
+
+            self.assertEqual(self.new_user.name,"Allan")
+            self.assertEqual(self.new_user.password,"Davis")
+
+     def test_save_user(self):
+             '''
+             Test case to test if the object is saved into the user user list
+             '''
+             self.new_user.save_user()
+             self.assertEqual (len(User.user_list),1)
+
+     def test_save multiple_user(self):
+             '''
+             test_save_multiple_contact
+
+
+
+if __name__ ==  '__main__':
+    unittest.main()
--- user.py
+++ user.py
@@ -3,9 +3,8 @@ class User:
     Class that generates new User.
 
     """
-    Userlist= []  #Array of empty user list
-    def _init_(self,name,site,password):
+    user_list= []  #Array of empty user list
+    def _init_(self,name,password):
 
         self.name=name
-        self.site=site
         self.password=password
