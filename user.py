c68a2c012c928730119e1e3fea5adf1ca945cea9
AndrewIndeche <indecheandrew@gmail.com>
2021-09-06 00:35:03 +0300

    Add pyperclip module

--- user.py
+++ user.py
@@ -8,3 +8,22 @@ class User:
 
         self.name=name
         self.password=password
+
+    def save_user(self):
+        User.user_list.append(self)
+
+
+    def delete_user(self):
+        '''
+        delete a user account
+        '''
+        User.user_list.remove(self)
+
+    @classmethod
+    def find_user(cls,name):
+        '''
+        Method that checks if a user exists from the user_list.
+        '''
+        for user in cls.user_list:
+            if user.name == name:
+                    return user
--- user_test.py
+++ user_test.py
@@ -63,5 +63,18 @@ class TestUser(unittest.Testcase):
         found_user = User.find_user("Jack","Sparrow")
         self.assertEqual(found_user.name, self.new_user.name)
 
+    def test_user_exists(self):
+        '''
+        Test to check if we can return a Boolean  if we cannot find the contact.
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Test","user") 
+        test_user.save_user()
+
+        user_exists = User.user_exist("0711223344")
+
+        self.assertTrue(user_exists)
+
 if __name__ ==  '__main__':
     unittest.main()
