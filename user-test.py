de6dcf681327f05f70557e217c726a7222c3e15c
AndrewIndeche <indecheandrew@gmail.com>
2021-09-05 18:14:51 +0300

    Add docstrings to define created classes

--- user-test.py
+++ user-test.py
@@ -1,2 +1,11 @@
 import unitest
 from user import User
+
+Class TestUser(unitest.Testcase):
+
+    '''
+    Test class that defines test cases for user class behaviours.
+
+    Args:
+        unitest.Testcase: TestCase class that helps in creating test cases
+    '''
