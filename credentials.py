9f0166c5c8f91e7ea9335b056cd47f4c3efef299
AndrewIndeche <indecheandrew@gmail.com>
2021-09-06 09:02:15 +0300

    Added empty array and defined class instance

--- credentials.py
+++ credentials.py
@@ -3,4 +3,10 @@ class credentials:
     '''
     class that generates instance of user credentials
     '''
+
+    credential_list= []
 def  __init __(self,media,email,password):
+
+    self.media=media
+    self.email=email
+    self.password=password
