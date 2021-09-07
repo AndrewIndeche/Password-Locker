afbb54533d64f3aa6319b052aeddb7effac368d5
AndrewIndeche <indecheandrew@gmail.com>
2021-09-07 08:48:17 +0300

    edits to errors

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from user import user
+from user import User
 from credentials import Credentials
 
 def create_user(name,password):
@@ -7,3 +7,132 @@ def create_user(name,password):
     '''
     new_user = User(name,password)
     return new_user
+
+def create_credentials(account, email, passlock):
+    '''
+    method credentials details
+    '''
+    new_credentials = Credentials(account, email, password)
+    return new_credentials
+
+def save_user(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
+
+def save_credentials(credentials):
+    '''
+    Function to save credentials
+    '''
+    credentials.save_credentials()
+
+def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
+
+def find_user(user):
+    '''
+    Function that finds a user
+    '''
+    return User.find_user(user)
+
+def display_credentials():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credentials.display_credentials()
+
+def main():
+    print("Hello Welcome to Password Locker,Your Tool to Edit and Save User credentials and passwords!What is your name?.  :- )  ")
+    name = input()
+
+    print(f"Hello {name}.")
+    print('\n')
+    while True:
+            print("Use these short codes to walk around the interface : cu - create a new user profile, da - display account,gp -generate password fu -find user, ex -exit the  interface")
+            short_code = input().lower()
+
+            if short_code == 'cu':
+                    print("New Account Initialization...")
+                    print(f"Done,{name}.Enter your Password")
+
+                    print("Password")
+                    password=input()
+
+
+                    save_user ( create_user(name,password))
+                    print('\n')
+                    print(f"New Account{name})created")
+
+                    print("Site that logs in you details")
+
+                    account=input()
+                    print("email")
+
+                    email=input()
+
+                    save_user ( create_credentials(name,email,password))
+                    print('\n')
+                    print(f"New Account{name} {email}.....{password})created")
+
+            elif short_code == "da":
+
+                    if display_credentials():
+                            print("Here are your account details")
+                            print("\n")
+
+                            for account in display_credentials():
+                                print(f"{name} {email} .....{password}")
+                                print('\n')
+
+                            else:
+                                    print('\n')
+                                    print("You dont seem to have any user details saved yet")
+                                    print('\n')
+
+            elif short_code == 'fu':
+
+                            print("Enter the user details you want to search for")
+
+                            search_user = input()
+                            if find_user(search_):
+                                    search_user = find_user(search_user)
+                                    print(f"{name} {user}.....{password}")
+                                    print('-' * 20)
+
+
+                            else:
+                                    print('\n')
+                                    print("That contact does not exist")
+                                    print('\n')
+
+            elif short_code == 'gp':
+
+                            print('\n')
+                            print("Generate a new password")
+                            letters= "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+                            how_many = len(letters)
+                            print("How long would you like your password to be? ")
+                            print(f"p.s: Maximum length of password is {how_many}")
+                            lent = int(input())
+                            password = "".join(random.sample(letters, lent))
+                            print(f"Your password has {lent} characters ")
+                            print(password)
+
+            elif short_code == 'ex':
+                            print('\n')
+                            print('Logging out')
+
+                            print('\n')
+                            print('\n')
+                            print("See you later Alligator")
+                            break
+    else:
+        print ("Use these short codes to walk around the interface : cu - create a new user profile, da - display account,gp -generate password fu -find user, ex -exit the  interface")
+
+if __name__ == '__main__':
+
+    main()
